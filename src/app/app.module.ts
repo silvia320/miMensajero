5ff7a9343c31a7ea259597c97632712c7602eee2
PCB <esilvia320@gmail.com>
2020-08-18 17:53:18 -0500

    14/8/2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SQLite } from '@ionic-native/sqlite';
+
 
 @NgModule({
   declarations: [AppComponent],
